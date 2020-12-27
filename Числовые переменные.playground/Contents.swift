0c8718f59b1875171ce2da9be7dad507f29d0836
iBlessme <76649553+iBlessme@users.noreply.github.com>
2020-12-27 15:27:47 +0300

    Add files via upload

---
+++ Числовые переменные.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+let one = 1 //Int
+let oneOne: Int = 1 //Int
+
+let two = 2.23 //Double
+let twoTwo: Double = 2.2323 //Double
+
+let three = 2.23 //Double
+let threeThree: Float = 2.23 //Float
+
+let oneInt = Int.max //9223372036854775807
+let twoInt = Int.min //-9223372036854775808
+
+let oneInt8 = Int8.max //127
+let twoInt8 = Int8.min //-128
+
+let oneInt16 = Int16.max //32767
+let twoInt16 = Int16.min //-32768
+
+let oneInt32 = Int32.max //2147483647
+let twoInt32 = Int32.min //-2147483648
+
+let oneUInt8 = UInt8.max // 255
+let twoUInt8 = UInt8.min // 0
